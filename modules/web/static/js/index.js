052b61b933515c6badbb290fe81c65f228275f0b
chawat <chawatyash@gmail.com>
2019-12-14 00:31:29 +0530

    Web interface started

---
+++ modules/web/static/js/index.js
@@ -0,0 +1,46 @@
+
+terminal = document.querySelector(".card-content");
+terminal.scrollTop = terminal.scrollHeight;
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+function sendVoice(str) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/voiceCtrl", true);
+  xhttp.onreadystatechange = function() {
+    if (!(this.readyState == 4 && this.status == 200)) {
+        alert("Server did not respond");
+    }
+  };
+  xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhttp.send("voiceStr=" + str);
+}
+
+
+function startVoice() {
+  console.log("Starting Voice Recording...");
+  if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.lang = "en-US";
+    recognition.start();
+
+    recognition.onresult = function(e) {
+      console.log(typeof(e.results[0][0].transcript));
+      // alert(e.results[0][0].transcript);
+      sendVoice(e.results[0][0].transcript);
+      recognition.stop();
+    };
+
+    recognition.onerror = function(e) {
+      recognition.stop();
+    }
+
+  }
+}
+    
+
+document.querySelector("button").onclick = startVoice;
